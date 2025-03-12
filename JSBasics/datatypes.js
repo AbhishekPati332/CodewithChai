0fff587cedc5e88cb84db330803ab55b2f79c82c
AbhishekPati332 <abhishekpatidar332@gmail.com>
2025-03-12 16:34:54 +0530

    First commit

---
+++ JSBasics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict";   //treat all js code as newer version 
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof age);
+
---
+++ JSBasics/variables.js
@@ -0,0 +1,13 @@
+const accountID = 144533
+let accountEmail = "abhishek@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+console.log(accountID)
+
+/*prefer not to use var
+bcoz of issue in block scope and functional scope
+*/
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
