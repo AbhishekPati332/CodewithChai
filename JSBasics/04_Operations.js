a31173b74e1a0b65e1585347e170c0b618eeba6f
AbhishekPati332 <abhishekpatidar332@gmail.com>
2025-03-15 20:34:35 +0530

    Updated code

---
+++ JSBasics/04_Operations.js
@@ -0,0 +1,26 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2/2)
+// console.log(4**2)
+
+let str1 = "hello"
+let str2 = "abhishek"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);  //string first hai to sabhi ko string mai
+console.log(1 + 2 + "2");   //string last mai toh pehle conversion ho jaega
+console.log((3 + 4) * 5 % 3);
+
+let gameCounter = 100
+gameCounter++ //++gamecounter;
+console.log(gameCounter)
+
+//prefix :- value pehle increment ho jati hai
+//postfix :- usage ke baad increment hoti hai
+// to study:- prefix and postfix js mdn
